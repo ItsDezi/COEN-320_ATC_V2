11813df299ab6b0d62f46edfaf8e80749eafaeab
Safaa Kadhim <66439492+safaaraed99@users.noreply.github.com>
2023-04-08 23:51:56 -0400

    Create DataDisplay.h

---
+++ src/DataDisplay.h
@@ -0,0 +1,21 @@
+#ifndef DATADISPLAY_H
+#define DATADISPLAY_H
+#include <iostream>
+#include <vector>
+#include "AircraftData.h"
+#include "ComputerSystem.h"
+using namespace std; 
+
+
+class DataDisplay {
+public:
+
+
+    void displayAircraftData(vector<AircraftData> aircraftData);
+
+    
+private: 
+
+};
+
+#endif 
