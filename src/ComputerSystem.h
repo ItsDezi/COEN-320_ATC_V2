ad5b99c0dd1eb58ccb8e522ac73a294d33ef85d1
Safaa Kadhim <66439492+safaaraed99@users.noreply.github.com>
2023-04-08 22:59:25 -0400

    ComputerSystem.h
    Works and outputs as required

--- src/ComputerSystem.h
+++ src/ComputerSystem.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <vector>
 #include <pthread.h>
-#include <mutex>
 #include "AircraftData.h"
 //#include "aircraft.h"
 //#include "Radar.cpp"
@@ -17,69 +16,55 @@ class ComputerSystem {
 
 public:
 
-    //constructor: We need to know the number of aircrafts in the air to determine if any violations occur
-	ComputerSystem(Radar& radar, int numberAircrafts, int periodicComputations);
-
+    //constructor: We need to know the number of aircrafts in the air to determine if any violations occur 
+	ComputerSystem(int periodicComputations);
+	
     //Destructor:
 	~ComputerSystem();
-
+    
     //To start the pthread for periodic computations to detect violations
-    void startPeriodicComputations();
-
+    void startPeriodicComputations(); 
+    
     //To stop the pthread for periodic computations to detect violation
-    void stopPeriodicComputations();
+    void stopPeriodicComputations(); 
 
-    //To calculate and detect Violations
-    void* computeViolations(vector<AircraftData>& aircraft);
-
-    //To display data on Display Data
-    void showDataDisplay();
-
-    //To update period
-    void updateAircraftPosition();
-
+	//To calculate and detect Violations
+    //const : to not modify contents in aricraft. 
+    // This function only reads -> no need to modify anything
+    bool computeViolations(vector<AircraftData> &aircraft);
+		
     //To emit an alert in case a violation occurs
-    void sendViolationAlert();
-
+	void sendViolationAlert();
+   
     //To calculate periodic computations
-    void periodicComputations();
+    void periodicComputations(); 
+   
+    //Function to calculate future position: 
+    void calculateFuturePosition(AircraftData* aircraft, double time_);
 
-    //To send updated Data to data display
-    void sendDataToDataDisplay(int ID,int x,int y,int z);
+    //Function to calculate the prediction for the next 3 minutes: 
+    void computeViolationsFor3Minutes(vector<AircraftData>& aircraft); 
 
-    //To send violation message to data display:
-    void sendViolationsToDataDisplay(string violation);
-
-    //To receive messages from Operator Console:
-   // void receiveMessage(Message message);
-
-    //To send updated xSpeed,ySpeed,zSpeed to Communication system:
-    void sendToCommunicationSystem(int ID, int xSpeed, int ySpeed, int zSpeed);
-
-private:
+private: 
 
     vector<AircraftData> aircraft;
 
-    //pthread to detect alert to operator:
+    //bool for computeViolations function: 
+    bool violationDetected ;
+
+    //pthread to detect alert to operator: 
     pthread_t detectViolations;
+   
+    // //Radar Instance 
+    // Radar& radar;
 
-    //Boolean function to stop detectViolation Thread:
-    bool falseFlag;
-
-    //Radar Instance
-    Radar& radar;
-
-    //DataDisplay Instance:
-    DataDisplay dataDisplay;
+    // //DataDisplay Instance: 
+    // DataDisplay dataDisplay;
 
     int numberAircrafts;
 
-    int periodicComputations_;
-
-    //pthread to protect shared data:
-    pthread_mutex_t aircraftMutex;
-    pthread_t aircraftMutex_;
-
+    int periodicComputations_; 
+    
 };
 
 #endif
